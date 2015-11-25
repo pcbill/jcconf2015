7c9d84ff9a85fa5d5314c0cc8c34ce9573973d61
pcbill <pcbill@gmail.com>
2015-11-26 02:54:03 +0800

    disable delete method, update id type in domain

--- src/main/java/com/example/domain/Book.java
+++ src/main/java/com/example/domain/Book.java
@@ -1,15 +1,34 @@
 package com.example.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Book {
 
     @Id
-    private String ISBN;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String name;
 
     private String author;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
--- src/main/java/com/example/domain/BookDao.java
+++ src/main/java/com/example/domain/BookDao.java
@@ -1,7 +1,12 @@
 package com.example.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface BookDao extends JpaRepository<Book, String>{
+public interface BookDao extends JpaRepository<Book, Long>{
+
+    @Override
+    @RestResource(exported=false)
+    void delete(Long id);
 
 }
